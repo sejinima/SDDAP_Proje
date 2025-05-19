85d35138b87112cd937febb6735afd1eac76165e
NerdSlayer1 <122046368+NerdSlayer1@users.noreply.github.com>
2025-05-19 21:39:13 +0300

    main.py'a frontend için değiştirildi

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from auth import router as auth_router
 from film import router as film_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # veya sadece "http://localhost:8080"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(film_router, prefix="/film")
