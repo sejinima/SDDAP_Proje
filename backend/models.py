7f3fecbbb90ca9cb6ee2042079e8d472ddae6dea
tariccc <trkhsmlr7@gmail.com>
2025-06-25 23:31:46 +0300

    Birleştirilmiş

---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class User(BaseModel):
+    username: str
+    password: str
+    user_type: str
+    email: str
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class Film(BaseModel):
+    id: int
+    title: str
+    year: int
+    rating: float
+    poster_url: Optional[str] = None
